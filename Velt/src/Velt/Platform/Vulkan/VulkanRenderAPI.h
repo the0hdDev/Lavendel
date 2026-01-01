d27505beb7fb954469656343a149875a9f40b402
Theo Wimber <theowimber@gmail.com>
2026-01-01 20:30:51 +0100

    idk

--- Velt/src/Velt/Platform/Vulkan/VulkanRenderAPI.h
+++ Velt/src/Velt/Platform/Vulkan/VulkanRenderAPI.h
@@ -1,2 +1,19 @@
 #pragma once
-#include "Renderer/RendererAPI.h"
+#include "Renderer/RenderAPI.h"
+#include "Core/Core.h"
+
+
+namespace Velt::Renderer::Vulkan {
+
+	class VELT_API VulkanRenderAPI : RenderAPI 
+	{
+	
+	public:
+		void Clear() override;
+		void SetClearColor(const glm::vec4& color) override;
+
+		void DrawIndexed(const std::shared_ptr<VertexBuffer>& vertexBuffer, std::shared_ptr<IndexBuffer>& indexBuffer) override;
+
+	};
+
+}
